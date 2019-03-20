4ec9f7767e08fa8ca663f137f20c6e4235dcb182
Travis <baksha97@gmail.com>
2019-03-20 14:54:52 -0400

    inst factory handler

--- src/instructions/InstructionFactory.java
+++ src/instructions/InstructionFactory.java
@@ -8,22 +8,26 @@ public class InstructionFactory {
     public Instructable getInstruction(String line, int lineNumber) {
         String[] parts = line.split(" ");
 
-        String possibleVarName = parts[0];
+        try {
+            String possibleVarName = parts[0];
 
-        if (parts[0].equalsIgnoreCase("if")) {
-            return new Conditional(line, parts, lineNumber);
-        } else if (parts[0].equalsIgnoreCase("GOTO")) {
-            return new GoToMacro(line, parts, lineNumber);
-        } else if (parts[2].equals("0")) {
-            return new SetZeroMacro(line, parts, lineNumber);
-        } else if (parts.length == 3) { //} else if (!parts[2].equals(possibleVarName) && parts.length == 3) {
-            return new CopyMacroOperator(line, parts, lineNumber);
-        } else if (parts[2].equals(possibleVarName) && parts[3].equals("+") && parts[4].equals("1")) {
-            return new Operator(line, possibleVarName, InstructionType.INCREMENT, lineNumber);
-        } else if (parts[2].equals(possibleVarName) && parts[3].equals("-") && parts[4].equals("1")) {
-            return new Operator(line, possibleVarName, InstructionType.DECREMENT, lineNumber);
-        } else {
-            throw new IllegalArgumentException("Cannot create Instruction with: " + Arrays.toString(parts));
+            if (parts[0].equalsIgnoreCase("if")) {
+                return new Conditional(line, parts, lineNumber);
+            } else if (parts[0].equalsIgnoreCase("GOTO")) {
+                return new GoToMacro(line, parts, lineNumber);
+            } else if (parts[2].equals("0")) {
+                return new SetZeroMacro(line, parts, lineNumber);
+            } else if (parts[1].equals("<-") && parts.length == 3) { //} else if (!parts[2].equals(possibleVarName) && parts.length == 3) {
+                return new CopyMacroOperator(line, parts, lineNumber);
+            } else if (parts[2].equals(possibleVarName) && parts[3].equals("+") && parts[4].equals("1")) {
+                return new Operator(line, possibleVarName, InstructionType.INCREMENT, lineNumber);
+            } else if (parts[2].equals(possibleVarName) && parts[3].equals("-") && parts[4].equals("1")) {
+                return new Operator(line, possibleVarName, InstructionType.DECREMENT, lineNumber);
+            }else{
+                throw new IllegalArgumentException();
+            }
+        }catch (Exception e){
+            throw new IllegalArgumentException("Cannot create Instruction with: " + Arrays.toString(parts) + " at " + lineNumber);
         }
     }
 }
